01bd12b85f393f8362a57d5ceb4f31b851ae9e2a
Daniel <toreno96@gmail.com>
2020-02-11 12:58:49 +0100

    Use separate database instance for tests
    It is more automated than solution commited in the
    3593f8bc90ba2ee70bbf16afb77dd8dec9571d75.
    Autotruncate is still not available, however, autoinit is (previously,
    manual creation and init of database was required).

--- backend/app/tests-start.sh
+++ backend/app/tests-start.sh
@@ -1,8 +1,14 @@
 #! /usr/bin/env bash
 set -e
 
-alembic upgrade head
-
+# Let the DB start
 python /app/app/tests_pre_start.py
 
+# Run migrations
+alembic upgrade head
+
+# Create initial data in DB
+python /app/app/initial_data.py
+
+# Run tests
 pytest $* /app/app/tests/
